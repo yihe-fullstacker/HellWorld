7ffbeea4f364da9f13779649ea8d81a28eae4e78
yihe-fullstacker <1712753438@qq.com>
2026-07-07 19:10:06 +0800

    A修改

--- Hellworld.java
+++ Hellworld.java
@@ -2,8 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Dev Branch");
-        System.out.println("Hello workspace2");
+        System.out.println("Hello A");
 
     }
 
